ae6a3a884cc694b5d4939737f0e8c0444cc9a3ac
Mark Woynarski <mark.woynarski@nobletdesign.com>
2020-06-30 12:51:40 -0600

    Added param for order direction in list query

--- src/Repository/AbstractAdminManagedRepository.php
+++ src/Repository/AbstractAdminManagedRepository.php
@@ -11,15 +11,17 @@ abstract class AbstractAdminManagedRepository extends ServiceEntityRepository
 {
     /**
      * @param string $order_by
+     * @param bool   $order
+     *
      * @return \Doctrine\ORM\Query
      */
-    public function getListQuery($order_by = ''): Query
+    public function getListQuery($order_by = '', $order = false): Query
     {
         $q = $this->createQueryBuilder('e');
 
         if($order_by)
         {
-            $q->orderBy($order_by, 'ASC');
+            $q->orderBy($order_by, $order ?: 'ASC');
         }
 
         return $q->getQuery();
